3ec33eaba33066853c9c63cd479f5023a3f3b9a3
evcareeria <erik.vesa.contact@gmail.com>
2025-01-08 17:15:55 +0200

    [Basic automation for logging into a service]

--- browser-automation/main.py
+++ browser-automation/main.py
@@ -1,1 +1,31 @@
-print("hello")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import time
+import sys
+
+arguments = sys.argv
+
+service = Service(executable_path="./chromedriver")
+driver = webdriver.Chrome(service=service)
+
+driver.get(arguments[1])
+login_button = driver.find_element(By.ID, "solo-login-btn")
+login_button.click()
+
+username = driver.find_element(By.ID, "username")
+password = driver.find_element(By.ID, "password")
+
+username.send_keys(arguments[2])
+password.send_keys(arguments[3])
+
+login_button = driver.find_element(By.CLASS_NAME, "login-page-primary-button")
+login_button.click()
+
+time.sleep(1)
+
+login_button = driver.find_element(By.CLASS_NAME, "login-page-primary-button")
+login_button.click()
+time.sleep(60)
+
+driver.quit()
