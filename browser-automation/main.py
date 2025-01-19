5016c5e7a01a8afdfe35a8c4d5c33465db7969ca
evcareeria <erik.vesa.contact@gmail.com>
2025-01-19 15:36:48 +0200

    [Changes to selenium script]
    - Started working with basic python course

--- browser-automation/main.py
+++ browser-automation/main.py
@@ -4,12 +4,18 @@ from selenium.webdriver.chrome.service import Service
 import time
 import sys
 
+# Empty old file
+f = open("demofile2.txt", "a")
+f.truncate(0)
+f.close()
+
 from helper_functions import btn_click_classname, btn_click_css_selector
 
 arguments = sys.argv
 
 service = Service(executable_path="./chromedriver")
 driver = webdriver.Chrome(service=service)
+driver.maximize_window()
 
 driver.get(arguments[1])
 login_button = driver.find_element(By.ID, "solo-login-btn")
@@ -27,6 +33,27 @@ btn_click_classname(driver, By, time, "login-page-primary-button")
 
 btn_click_css_selector(driver, By, time, "button.dashboard-show-bank-statement")
 
-time.sleep(60)
+# Parent div /html/body/div[1]/div[1]/div[2]/div/div/div[2]
+
+# Child div /html/body/div[1]/div[1]/div[2]/div/div/div[2]/div[1]
+
+selected_months = driver.find_elements(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div/div/div[2]')
+print(selected_months)
+
+f = open("demofile2.txt", "a")
+
+for month in selected_months:
+    print(month.text)
+    f.write(month.text)
+
+    
+#for month in selected_months:
+#    btn_click_css_selector(driver, By, time, "button.btn-expand-month btn btn-default")
+#    items = driver.find_element(By.CLASS_NAME, "list-items")
+#    print(items)
+
+#list-item status-ok clickable
+
+time.sleep(300)
 
 driver.quit()
---
+++ exercises/1/main.py
@@ -0,0 +1,11 @@
+# Band name gen
+
+# input
+print("What's the name of the city you grew up in?")
+input1 = input()
+
+print("What's your pet's name?")
+input2 = input()
+
+# output
+print(f'Your band name could be {input1} {input2}')
